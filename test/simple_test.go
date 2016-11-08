b19fd40a6fe053eef07f4cf3aaf1c1291bae0057
ErgoZ Vaper <ergozru@gmail.com>
2016-11-08 13:53:05 +0300

    try to fix build

--- test/simple_test.go
+++ test/simple_test.go
@@ -9,15 +9,13 @@ import (
 //TestSomething Test method 1
 func TestSomething(t *testing.T) {
 	ass := assert.New(t)
-
 	// assert equality
 	ass.Equal(123, 123, "they should be equal")
-
-	// assert inequality
-	ass.NotEqual(123, 456, "they should not be equal")
 }
 
 //TestAverage Test method 2
 func TestAverage(t *testing.T) {
-
+	ass := assert.New(t)
+	// assert inequality
+	ass.NotEqual(123, 456, "they should not be equal")
 }
